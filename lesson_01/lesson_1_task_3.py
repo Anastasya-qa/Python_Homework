8f78c456285628b2fc424b5d00380c8ee942eb86
Anastasya_qa <nasta11012005@gmail.com>
2026-07-14 21:58:24 +0300

    update

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,4 @@
+last_name = input("Введите Вашу фамилию: ")
 first_name = input("Введите Ваше имя: ")
-last_name = input("Введите Вашу фамилию: А")
 
-print("Вас зовут:", first_name, last_name)
+print("Вас зовут:", last_name, first_name)
