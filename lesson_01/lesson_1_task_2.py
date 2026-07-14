c10cdf635b2b07fee9ca5a5bb07cc50b5c0eab9a
Anastasya_qa <nasta11012005@gmail.com>
2026-07-15 01:52:07 +0300

    full_update

--- lesson_01/lesson_1_task_2.py
+++ lesson_01/lesson_1_task_2.py
@@ -1,4 +1,4 @@
 my_age = 21
 my_age = 24
-
+  
 print(my_age)
--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,4 @@
 last_name = input("Введите Вашу фамилию: ")
 first_name = input("Введите Ваше имя: ") 
-
+  
 print("Вас зовут:", last_name, first_name)
